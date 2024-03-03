a2a4fde47e021f72a132b5ee6fddd5092af8375a
fuadhasen <fuadhas6634@gmail.com>
2024-03-03 12:44:00 +0530

    bubble sort update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,27 +1,28 @@
 #include "sort.h"
 
-/*
+/**
+ * bubble_sort - bubble sorting algorithm(swap each other)
+ * @array: the object to be sorted
+ * @size: size of the array
  *
- *
- *
- *
+ * Return: void
  */
 
- void bubble_sort(int *array, size_t size)
- {
-    size_t temp, i, j;
+void bubble_sort(int *array, size_t size)
+{
+	size_t temp, i, j;
 
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                print_array(array, size);
-            }
-        }
-    }
- }
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
