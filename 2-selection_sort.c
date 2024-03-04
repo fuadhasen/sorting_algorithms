f27cccc16fa9b98ea6b970661fdcf61ad1692d28
fuadhasen <fuadhas6634@gmail.com>
2024-03-05 00:51:17 +0530

    selection sort created

---
+++ 2-selection_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * selection_sort - select min wrt to beginning and swap
+ * @array: to be sorted
+ * @size: length of array
+ *
+ * Return: void
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int small, temp;
+
+	for (j = 0; j < size - 1; j++)
+	{
+		small = j;
+		for (i = j + 1; i < size; i++)
+		{
+			if (array[i] < array[small])
+			{
+				small = i;
+			}
+		}
+		temp = array[j];
+		array[j] = array[small];
+		array[small] = temp;
+		print_array(array, size);
+	}
+}
